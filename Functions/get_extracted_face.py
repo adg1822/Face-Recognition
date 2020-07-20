f420232204014fda1c58b89818dc96b1c525fd65
adg1822 <56107124+adg1822@users.noreply.github.com>
2020-07-20 15:30:08 +0530

    extract face

---
+++ Functions/get_extracted_face.py
@@ -0,0 +1,38 @@
+#install MTCNN
+$ pip install mtcnn
+
+import mtcnn
+from os import listdir
+from os.path import isdir
+from PIL import Image
+from matplotlib import pyplot
+from numpy import savez_compressed
+from numpy import asarray
+from mtcnn.mtcnn import MTCNN
+import numpy as np
+
+# extract a single face from a given photograph
+def extract_face(path, required_size=(160, 160)):
+	# load image from file
+	image = Image.open(path)
+	# convert to RGB, if needed
+	image = image.convert('RGB')
+	# convert to array
+	pixels = asarray(image)
+	# create the detector, using default weights
+	detector = MTCNN()
+	# detect faces in the image
+	results = detector.detect_faces(pixels)
+	# extract the bounding box from the first face
+	x1, y1, width, height = results[0]['box']
+	# bug fix
+	x1, y1 = abs(x1), abs(y1)
+	x2, y2 = x1 + width, y1 + height
+	# extract the face
+	face = pixels[y1:y2, x1:x2]
+	# resize pixels to the model size
+	image = Image.fromarray(face)
+	image = image.resize(required_size)
+	face_array = asarray(image)
+	return face_array
+  
